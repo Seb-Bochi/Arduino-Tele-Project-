db6315ce7b3525e3162fdd41d2f09f2eabe2b68f
Hakon2003DTU <hakonhlyn@gmail.com>
2025-01-17 14:01:49 +0100

    Add files via upload

--- Master_MCU/Master_MCU.ino
+++ Master_MCU/Master_MCU.ino
@@ -21,17 +21,18 @@ bool burglary = false;
 int alarm=false;
 int prev_buttonState = false;
 
-
-typedef struct Device2_message {
-    int Detected_Light;
-    int RFID_Access;
-    String UID;
-} Device2_message;
-
 typedef struct Device1_message {
     int Detected_Sound;
     int Detected_Motion;
 } Device1_message;
+
+typedef struct Device2_message {
+    int RFID_Access;
+    int Detected_Light;
+    String UID;
+} Device2_message;
+
+
 
 
 // Create a struct_message called myData
@@ -48,20 +49,21 @@ void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
   
    // Example: Act based on sender's MAC address
     if (strcmp(macStr, "C8:C9:A3:69:D3:F9") == 0) {
-         memcpy(&Device1, incomingData, sizeof(Device1));
+        memcpy(&Device1, incomingData, sizeof(Device1));
         Serial.println("Action for Device 1");
         Serial.print("Detected_Sound: ");
         Serial.println(Device1.Detected_Sound);
         Serial.print("Detected_Motion: ");
         Serial.println(Device1.Detected_Motion);
     }   
-    if (strcmp(macStr, "YY:YY:YY:YY:YY:YY") == 0){ // change it to the mac address for the EPS sender 
+    if (strcmp(macStr, "C4:5B:BE:74:50:EF") == 0){ // Data from RFID and Light sensor 
         // Perform specific action for this sender
+        memcpy(&Device2, incomingData, sizeof(Device2));
         Serial.println("Action for Device 2");
+        Serial.print("RFID_Access: ");
+        Serial.println(Device2.RFID_Access);
         Serial.print("Detected_Light: ");
         Serial.println(Device2.Detected_Light);
-        Serial.print("Detected_Light: ");
-        Serial.println(Device2.RFID_Access);
         Serial.print("UID: ");
         Serial.println(Device2.UID); 
     }   
@@ -101,42 +103,80 @@ void setup() {
 }
 
 void loop() {
+// Check if the alarm is off and handle the button press to activate it
+  if (alarm == false) {
+    alarm_off(ledPinRed, ledPinGreen, buzzerPin);
+    alarm = check_button(ButtonPin); // Check if the button is pressed to turn on the alarm
+  } 
+  else {
+    // Alarm is ON, process incoming messages or burglary status
+    if (New_messege || burglary) {
+      delay(10);
+      New_messege = false;
 
-  if (alarm == false){
+      // Check if RFID is present to turn off the alarm
+      if (Device2.RFID_Access == 1) {
+        burglary = false; // Reset burglary flag
+        alarm = false;    // Turn off the alarm
+        alarm_off(ledPinRed, ledPinGreen, buzzerPin);
+        Device1.Detected_Sound=0;
+        Device1.Detected_Motion=0;
+        Device2.Detected_Light =0;
+        Device2.RFID_Access=0;
+        // Optionally send the UID to ThingSpeak
+        //setUIDString(3, Device2.UID); // Sending UID to ThingSpeak (Field 3)
+      }
+      else if ((Device1.Detected_Sound == 1) || (Device1.Detected_Motion == 1) || (Device2.Detected_Light == 1) || burglary) {
+                  alarm_sound(ledPinRed, ledPinGreen, buzzerPin); // Trigger the alarm sound and lights
+                  burglary = true;  // Mark that a burglary occurred
+                  //alarm = true;     // Keep the alarm on
+                  } 
+    } 
+    else {
+      alarm_on_sound_but_no_sound(ledPinRed, ledPinGreen, buzzerPin);
+    }
+  }
+
+  // Periodically update ThingSpeak
+  if (count >= postDelay) {
+    setAlarm((bool)alarm); // Update alarm state on ThingSpeak (Field 2)
+    sendUpdate();          // Push data to ThingSpeak
+    count = 0;             // Reset counter
+  }
+ /* if (alarm == false){
         //Serial.println(alarm);
         alarm_off(ledPinRed,ledPinGreen, buzzerPin);
         alarm=check_button(ButtonPin);
         //check if button is pressed to put alarm on   
       }
   else{
-    alarm_on_sound_but_no_sound(ledPinRed,ledPinGreen, buzzerPin);
-  }
-
-
-  if (New_messege){ // gets a messenge from the other esp
-    if (alarm==true){  
+    if (New_messege ||burglary ){ // gets a messenge from the other esp
+      New_messege=false; 
       if (Device2.RFID_Access == 1){
          burglary=false;
          alarm=false;
          // send the UID to thingspeak
-         setUIDString(3, Device2.UID);
-
+         //setUIDString(3, Device2.UID);
          alarm_off(ledPinRed,ledPinGreen, buzzerPin);
          
-       }
+      }
       if(Device1.Detected_Sound == 1 || Device1.Detected_Motion == 1 || Device2.Detected_Light == 1|| burglary==true ){
         alarm_sound(ledPinRed,ledPinGreen, buzzerPin); // makes lights blik and buzzer makes sound (alarm has been trigered) 
-        burglary=true; 
-        }
-       
-       else{
-         alarm_on_sound_but_no_sound(ledPinRed,ledPinGreen, buzzerPin);
-       }
-    }   
-  New_messege=false; 
+        burglary=true;
+        alarm=true; 
+      }
+
+  }
+  else{
+      alarm_on_sound_but_no_sound(ledPinRed,ledPinGreen, buzzerPin);
+      }
   }
 
+
+  
+
   if(count == postDelay){
+    setAlarm((bool)alarm);
     sendUpdate();
     count = 0;
   }
@@ -144,9 +184,25 @@ void loop() {
   //client.stop();
 
   delay(1);
-  count++;
+  count++;*/
 }
 
+void setDataInt(unsigned int field, int value){
+  int x = 0;
+
+  x =  ThingSpeak.setField(field, value);
+
+  if (x == 200){
+    Serial.println("setDataInt: Data set succesfully!");
+  }else{
+    Serial.print("setDataInt: Error ");
+    Serial.println(x);
+  }
+}
+
+void setAlarm(bool state){
+  setDataInt(2, (int)state);
+}
 
 void initAlarm(int Red_LedPin,int Green_LedPin, int BuzzerPin,int Button_Pin){
   pinMode(Button_Pin, INPUT_PULLUP);
@@ -201,9 +257,9 @@ void setUIDString(unsigned int field, String str){
   x =  ThingSpeak.setField(field, str);
 
   if (x == 200){
-    Serial.println("setUIDString: Data set succesfully!");
+    Serial.println("setDataString: Data set succesfully!");
   }else{
-    Serial.print("setUIDString: Error ");
+    Serial.print("setDataString: Error ");
     Serial.println(x);
   }
 }
